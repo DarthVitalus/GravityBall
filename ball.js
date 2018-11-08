ef85a1ad70778e173e55ebd90d091c59d79f3b3d
Абрамов Виталий <v.abramov@action-media.ru>
2018-11-08 21:38:49 +0300

    ver 1.0

--- ball.js
+++ ball.js
@@ -9,47 +9,79 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var Penetration;
+(function (Penetration) {
+    Penetration["On"] = "on";
+    Penetration["Off"] = "off";
+})(Penetration || (Penetration = {}));
 var calculatePageBorder = function () {
+    var borderSpace = 0;
     pageBorder = {
-        top: 0,
-        right: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-        bottom: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-        left: 0
+        top: borderSpace,
+        right: Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - borderSpace,
+        bottom: Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - borderSpace,
+        left: borderSpace,
     };
     console.log(pageBorder);
     sq.style.width = pageBorder.right - 4 + 'px';
     sq.style.height = pageBorder.bottom - 4 + 'px';
 };
 var validateCoordsByPage = function (el, coords, shift) {
-    var elBorders = {
+    if (shift === void 0) { shift = { x: 0, y: 0 }; }
+    var newElBorders = {
         top: coords.y - shift.y,
         right: coords.x + (el.offsetWidth - shift.x),
         bottom: coords.y + (el.offsetHeight - shift.y),
-        left: coords.x - shift.x
+        left: coords.x - shift.x,
     };
-    var newCoords = __assign({}, coords, { changed: true });
-    if (elBorders.top < pageBorder.top) {
+    var newCoords = __assign({}, coords, { reachedSide: false, reachedUpside: false, reachedTop: false, reachedRight: false, reachedLeft: false, reachedBottom: false });
+    if (newElBorders.top < pageBorder.top) {
         newCoords.y = pageBorder.top;
+        newCoords.reachedTop = true;
+        newCoords.reachedUpside = true;
     }
-    else if (elBorders.right > pageBorder.right) {
+    if (newElBorders.right > pageBorder.right) {
         newCoords.x = pageBorder.right - el.offsetWidth;
+        newCoords.reachedRight = true;
+        newCoords.reachedSide = true;
     }
-    else if (elBorders.bottom > pageBorder.bottom) {
+    if (newElBorders.bottom > pageBorder.bottom) {
         newCoords.y = pageBorder.bottom - el.offsetHeight;
+        newCoords.reachedBottom = true;
+        newCoords.reachedUpside = true;
     }
-    else if (elBorders.left < pageBorder.left) {
+    if (newElBorders.left < pageBorder.left) {
         newCoords.x = pageBorder.left;
-    }
-    else {
-        newCoords.changed = false;
+        newCoords.reachedLeft = true;
+        newCoords.reachedSide = true;
     }
     return newCoords;
 };
-var drag = function (el, newCoords, shift) {
+var move = function (el, newCoords, shift) {
+    if (shift === void 0) { shift = { x: 0, y: 0 }; }
     var validCoords = validateCoordsByPage(el, newCoords, shift);
-    el.style.left = validCoords.x - (validCoords.changed ? 0 : shift.x) + 'px';
-    el.style.top = validCoords.y - (validCoords.changed ? 0 : shift.y) + 'px';
-    if (validCoords.changed) {
+    var newValidCoords = {
+        x: validCoords.x - (validCoords.reachedSide ? 0 : shift.x),
+        y: validCoords.y - (validCoords.reachedUpside ? 0 : shift.y),
+    };
+    el.style.left = newValidCoords.x + 'px';
+    el.style.top = newValidCoords.y + 'px';
+    if (!changingCoords) {
+        changingCoords = {
+            prevCoords: __assign({}, newValidCoords),
+            curCoords: __assign({}, newValidCoords),
+            deltaX: 0,
+            deltaY: 0,
+        };
+    }
+    changingCoords.prevCoords = __assign({}, changingCoords.curCoords);
+    changingCoords.curCoords = __assign({}, newValidCoords);
+    changingCoords.deltaX = changingCoords.curCoords.x - changingCoords.prevCoords.x;
+    changingCoords.deltaY = changingCoords.curCoords.y - changingCoords.prevCoords.y;
+    if ((validCoords.reachedTop && changingCoords.deltaY < 0) ||
+        (validCoords.reachedBottom && changingCoords.deltaY > 0) ||
+        (validCoords.reachedLeft && changingCoords.deltaX < 0) ||
+        (validCoords.reachedRight && changingCoords.deltaX < 0)) {
         drop(el);
     }
 };
@@ -57,41 +89,113 @@ var drop = function (el) {
     console.log('dropping');
     document.onmousemove = null;
     el.onmouseup = null;
+    startFalling(el);
 };
 var getCoords = function (elem) {
     var box = elem.getBoundingClientRect();
     return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
+        y: box.top + pageYOffset,
+        x: box.left + pageXOffset,
     };
 };
+var drag = function (e, shift, el) {
+    var newCoords2 = {
+        x: e.pageX,
+        y: e.pageY,
+    };
+    move(el, newCoords2, shift);
+};
 var startDrag = function (el, e) {
+    clearInterval(falling);
     var coords = getCoords(el);
     var shift = {
-        x: e.pageX - coords.left,
-        y: e.pageY - coords.top
+        x: e.pageX - coords.x,
+        y: e.pageY - coords.y,
     };
-    var newCoords = {
-        x: e.pageX,
-        y: e.pageY
-    };
-    console.log('start', newCoords, shift, validateCoordsByPage(el, newCoords, shift));
-    drag(el, newCoords, shift);
-    document.onmousemove = function (e) {
-        var newCoords = {
-            x: e.pageX,
-            y: e.pageY
-        };
-        console.log('move', newCoords, shift, validateCoordsByPage(el, newCoords, shift));
-        drag(el, newCoords, shift);
+    drag(e, shift, el);
+    document.onmousemove = function (event1) {
+        drag(event1, shift, el);
     };
     el.onmouseup = function () {
         drop(el);
     };
 };
+var startFalling = function (el) {
+    console.log('falling');
+    var gravity = 500;
+    var timeDeltaS = 0.01;
+    var resistanceResistance = 0.997;
+    var resistanceY = 0.96;
+    var resistanceX = 0.992;
+    var velocityY = Math.abs(changingCoords.deltaY * 2 / timeDeltaS);
+    var directionX = 1;
+    var directionY = changingCoords.deltaY >= 0 ? 1 : -1;
+    var deltaX = changingCoords.deltaX;
+    var curCoords = getCoords(el);
+    falling = setInterval(function () {
+        rotate(el, rotationDeg);
+        var velocity = getNewVelocity(gravity, timeDeltaS);
+        velocityY = velocityY + velocity * directionY;
+        velocityY *= resistanceY;
+        deltaX *= resistanceX;
+        rotationDeg += deltaX * directionX;
+        if (rotationDeg > 360) {
+            rotationDeg -= 360;
+        }
+        var distanceY = getDistance(velocityY, timeDeltaS);
+        var newCoords = {
+            x: curCoords.x + deltaX * directionX,
+            y: curCoords.y + distanceY * directionY,
+        };
+        var validCoords = validateCoordsByPage(el, newCoords);
+        if (wallPenetration === Penetration.On && validCoords.reachedSide) {
+            console.log('CLINK!');
+            clearInterval(falling);
+        }
+        directionX = validCoords.reachedSide ? directionX * -1 : directionX;
+        directionY = validCoords.reachedUpside ? directionY * -1 : directionY;
+        move(el, validCoords);
+        curCoords = __assign({}, validCoords);
+        // reached floor
+        if (validCoords.reachedUpside && directionY < 0) {
+            resistanceX *= resistanceResistance;
+            resistanceY *= resistanceResistance;
+            // almost stopped
+            if (deltaX < 1 && velocityY < velocity) {
+                console.log('stop fall');
+                clearInterval(falling);
+            }
+        }
+        // reached highest point
+        if (directionY < 0 && velocityY < velocity) {
+            velocityY = 0;
+            directionY = 1;
+        }
+    }, timeDeltaS * 1000);
+};
+var getNewVelocity = function (gravity, timeDeltaS) {
+    return Math.pow(gravity * timeDeltaS, 3);
+};
+var getDistance = function (velocity, timeDeltaS) {
+    return 1 / 2 * velocity * timeDeltaS;
+};
+var rotate = function (el, degreeNumber) {
+    el.style.transform = "rotate(" + degreeNumber + "deg)";
+};
+// program starts
 var ball = document.getElementById('ball');
 var sq = document.getElementById('square');
-var pageBorder = undefined;
+var wallPenetrationEl = document.getElementById('wallPenetration');
+var wallPenetrationToggle = document.getElementById('wallPenetrationToggle');
+var pageBorder;
+var changingCoords;
+var falling;
+var rotationDeg = 0;
+var wallPenetration = wallPenetrationEl.innerHTML;
+wallPenetrationToggle.onclick = function () {
+    wallPenetration = wallPenetration === Penetration.On ? Penetration.Off : Penetration.On;
+    wallPenetrationEl.innerHTML = wallPenetration;
+};
 ball.ondragstart = function () {
     return false;
 };
@@ -101,16 +205,18 @@ ball.onmousedown = function (e) {
 document.body.onresize = function () {
     calculatePageBorder();
     var coords = getCoords(ball);
-    drag(ball, {
-        x: coords.left,
-        y: coords.top,
-    }, { x: 0, y: 0 });
+    var validCoords = validateCoordsByPage(ball, coords);
+    if (validCoords.reachedSide || validCoords.reachedUpside) {
+        move(ball, validCoords, { x: 0, y: 0 });
+    }
 };
 window.onload = function () {
     calculatePageBorder();
-    drag(ball, {
+    move(ball, {
         x: pageBorder.right / 2 - ball.offsetWidth,
         y: pageBorder.bottom / 2 - ball.offsetHeight,
     }, { x: 0, y: 0 });
+    startFalling(ball);
+    rotate(ball, rotationDeg);
 };
 //# sourceMappingURL=ball.js.map
--- ball.ts
+++ ball.ts
@@ -1,128 +1,279 @@
 interface ICoords {
-    x: number;
-    y: number;
+	x: number;
+	y: number;
+}
+interface IChangingCoords {
+	prevCoords: ICoords;
+	curCoords: ICoords;
+	deltaX: number;
+	deltaY: number;
 }
 interface INewCoords extends ICoords {
-    changed: boolean;
+	reachedSide: boolean;
+	reachedUpside: boolean;
+	reachedTop: boolean;
+	reachedRight: boolean;
+	reachedLeft: boolean;
+	reachedBottom: boolean;
+}
+interface ISceneBorder {
+	top: number;
+	right: number;
+	left: number;
+	bottom: number;
+}
+enum Penetration {
+	On = 'on',
+	Off = 'off',
 }
 
 const calculatePageBorder = (): void => {
-    pageBorder = {
-        top: 0,
-        right: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-        bottom: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-        left: 0
-    };
-    console.log(pageBorder);
-    sq.style.width = pageBorder.right - 4 + 'px';
-    sq.style.height = pageBorder.bottom - 4 + 'px';
+	const borderSpace = 0;
+	pageBorder = {
+		top: borderSpace,
+		right: Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - borderSpace,
+		bottom: Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - borderSpace,
+		left: borderSpace,
+	};
+	console.log(pageBorder);
+	sq.style.width = pageBorder.right - 4 + 'px';
+	sq.style.height = pageBorder.bottom - 4 + 'px';
 };
 
-const validateCoordsByPage = (el: HTMLElement, coords: ICoords, shift: ICoords): INewCoords => {
-    const elBorders = {
-        top: coords.y - shift.y,
-        right: coords.x + (el.offsetWidth - shift.x),
-        bottom: coords.y + (el.offsetHeight - shift.y),
-        left: coords.x - shift.x
-    };
+const validateCoordsByPage = (el: HTMLElement, coords: ICoords, shift: ICoords = {x: 0, y: 0}): INewCoords => {
+	const newElBorders = {
+		top: coords.y - shift.y,
+		right: coords.x + (el.offsetWidth - shift.x),
+		bottom: coords.y + (el.offsetHeight - shift.y),
+		left: coords.x - shift.x,
+	};
 
-    const newCoords = {
-        ...coords,
-        changed: true
-    };
+	const newCoords: INewCoords = {
+		...coords,
+		reachedSide: false,
+		reachedUpside: false,
+		reachedTop: false,
+		reachedRight: false,
+		reachedLeft: false,
+		reachedBottom: false,
+	};
 
-    if (elBorders.top < pageBorder.top) {
-        newCoords.y = pageBorder.top;
-    } else if (elBorders.right > pageBorder.right) {
-        newCoords.x = pageBorder.right - el.offsetWidth;
-    } else if (elBorders.bottom > pageBorder.bottom) {
-        newCoords.y = pageBorder.bottom - el.offsetHeight;
-    } else if (elBorders.left < pageBorder.left) {
-        newCoords.x = pageBorder.left;
-    } else {
-        newCoords.changed = false;
-    }
+	if (newElBorders.top < pageBorder.top) {
+		newCoords.y = pageBorder.top;
+		newCoords.reachedTop = true;
+		newCoords.reachedUpside = true;
+	}
+	if (newElBorders.right > pageBorder.right) {
+		newCoords.x = pageBorder.right - el.offsetWidth;
+		newCoords.reachedRight = true;
+		newCoords.reachedSide = true;
+	}
+	if (newElBorders.bottom > pageBorder.bottom) {
+		newCoords.y = pageBorder.bottom - el.offsetHeight;
+		newCoords.reachedBottom = true;
+		newCoords.reachedUpside = true;
+	}
+	if (newElBorders.left < pageBorder.left) {
+		newCoords.x = pageBorder.left;
+		newCoords.reachedLeft = true;
+		newCoords.reachedSide = true;
+	}
 
-    return newCoords;
+	return newCoords;
 };
 
-const drag = (el: HTMLElement, newCoords: ICoords, shift: ICoords): void => {
-    const validCoords = validateCoordsByPage(el, newCoords, shift);
-    el.style.left = validCoords.x - (validCoords.changed ? 0 : shift.x) + 'px';
-    el.style.top = validCoords.y - (validCoords.changed ? 0 : shift.y) + 'px';
-    if (validCoords.changed) {
-        drop(el);
-    }
+const move = (el: HTMLElement, newCoords: ICoords, shift: ICoords = {x: 0, y: 0}): void => {
+	const validCoords = validateCoordsByPage(el, newCoords, shift);
+	const newValidCoords = {
+		x: validCoords.x - (validCoords.reachedSide ? 0 : shift.x),
+		y: validCoords.y - (validCoords.reachedUpside ? 0 : shift.y),
+	};
+	el.style.left = newValidCoords.x + 'px';
+	el.style.top = newValidCoords.y + 'px';
+
+	if (!changingCoords) {
+		changingCoords = {
+			prevCoords: {...newValidCoords},
+			curCoords: {...newValidCoords},
+			deltaX: 0,
+			deltaY: 0,
+		};
+	}
+	changingCoords.prevCoords = {...changingCoords.curCoords};
+	changingCoords.curCoords = {...newValidCoords};
+	changingCoords.deltaX = changingCoords.curCoords.x - changingCoords.prevCoords.x;
+	changingCoords.deltaY = changingCoords.curCoords.y - changingCoords.prevCoords.y;
+
+	if (
+		(validCoords.reachedTop && changingCoords.deltaY < 0) ||
+		(validCoords.reachedBottom && changingCoords.deltaY > 0) ||
+		(validCoords.reachedLeft && changingCoords.deltaX < 0) ||
+		(validCoords.reachedRight && changingCoords.deltaX < 0)
+	) {
+		drop(el);
+	}
 };
 
 const drop = (el: HTMLElement): void => {
-    console.log('dropping');
-    document.onmousemove = null;
-    el.onmouseup = null;
+	console.log('dropping');
+	document.onmousemove = null;
+	el.onmouseup = null;
+	startFalling(el);
 };
 
-const getCoords = (elem): {top,left} => {
-    const box = elem.getBoundingClientRect();
-    return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
-    };
+const getCoords = (elem: HTMLElement): ICoords => {
+	const box = elem.getBoundingClientRect();
+	return {
+		y: box.top + pageYOffset,
+		x: box.left + pageXOffset,
+	};
+};
+
+const drag = (e: MouseEvent, shift: ICoords, el: HTMLElement): void => {
+	const newCoords2: ICoords = {
+		x: e.pageX,
+		y: e.pageY,
+	};
+
+	move(el, newCoords2, shift);
 };
 
 const startDrag = (el: HTMLElement, e: MouseEvent): void => {
-    const coords = getCoords(el);
-    const shift: ICoords = {
-        x: e.pageX - coords.left,
-        y: e.pageY - coords.top
-    };
-    const newCoords: ICoords = {
-        x: e.pageX,
-        y: e.pageY
-    };
+	clearInterval(falling);
+	const coords: ICoords = getCoords(el);
+	const shift: ICoords = {
+		x: e.pageX - coords.x,
+		y: e.pageY - coords.y,
+	};
+	drag(e, shift, el);
 
-    console.log('start', newCoords, shift, validateCoordsByPage(el, newCoords, shift));
-    drag(el, newCoords, shift);
+	document.onmousemove = (event1: MouseEvent): void => {
+		drag(event1, shift, el);
+	};
 
-    document.onmousemove = (e: MouseEvent): void => {
-        const newCoords: ICoords = {
-            x: e.pageX,
-            y: e.pageY
-        };
-        console.log('move', newCoords, shift, validateCoordsByPage(el, newCoords, shift));
-
-        drag(el, newCoords, shift);
-    };
-
-    el.onmouseup = (): void => {
-        drop(el);
-    };
+	el.onmouseup = (): void => {
+		drop(el);
+	};
 };
 
+const startFalling = (el: HTMLElement): void => {
+	console.log('falling');
+	const gravity: number = 500;
+	const timeDeltaS: number = 0.01;
+	const resistanceResistance: number = 0.997;
+	let resistanceY: number = 0.96;
+	let resistanceX: number = 0.992;
+	let velocityY: number = Math.abs(changingCoords.deltaY * 2 / timeDeltaS);
+	let directionX: number = 1;
+	let directionY: number = changingCoords.deltaY >= 0 ? 1 : -1;
+	let deltaX = changingCoords.deltaX;
+	let curCoords: ICoords = getCoords(el);
+
+	falling = setInterval(
+		() => {
+			rotate(el, rotationDeg);
+
+			const velocity = getNewVelocity(gravity, timeDeltaS);
+			velocityY = velocityY + velocity * directionY;
+
+			velocityY *= resistanceY;
+			deltaX *= resistanceX;
+			rotationDeg += deltaX * directionX;
+			if (rotationDeg > 360) {
+				rotationDeg -= 360;
+			}
+
+			const distanceY = getDistance(velocityY, timeDeltaS);
+			const newCoords = {
+				x: curCoords.x + deltaX * directionX,
+				y: curCoords.y + distanceY * directionY,
+			};
+			const validCoords = validateCoordsByPage(el, newCoords);
+
+			if (wallPenetration === Penetration.On && validCoords.reachedSide) {
+				console.log('CLINK!');
+				clearInterval(falling);
+			}
+
+			directionX = validCoords.reachedSide ? directionX * -1 : directionX;
+			directionY = validCoords.reachedUpside ? directionY * -1 : directionY;
+
+			move(el, validCoords);
+
+			curCoords = {...validCoords};
+
+			// reached floor
+			if (validCoords.reachedUpside && directionY < 0) {
+				resistanceX *= resistanceResistance;
+				resistanceY *= resistanceResistance;
+
+				// almost stopped
+				if (deltaX < 1 && velocityY < velocity) {
+					console.log('stop fall');
+					clearInterval(falling);
+				}
+			}
+
+			// reached highest point
+			if (directionY < 0 && velocityY < velocity) {
+				velocityY = 0;
+				directionY = 1;
+			}
+		}, timeDeltaS * 1000);
+};
+
+const getNewVelocity = (gravity: number, timeDeltaS: number): number => {
+	return Math.pow(gravity * timeDeltaS, 3);
+};
+
+const getDistance = (velocity: number, timeDeltaS: number): number => {
+	return 1 / 2 * velocity * timeDeltaS;
+};
+
+const rotate = (el: HTMLElement, degreeNumber: number): void => {
+	el.style.transform = `rotate(${degreeNumber}deg)`;
+};
+
+// program starts
 const ball = document.getElementById('ball');
 const sq = document.getElementById('square');
-let pageBorder = undefined;
+const wallPenetrationEl = document.getElementById('wallPenetration');
+const wallPenetrationToggle = document.getElementById('wallPenetrationToggle');
+let pageBorder: ISceneBorder;
+let changingCoords: IChangingCoords;
+let falling;
+let rotationDeg = 0;
+let wallPenetration = wallPenetrationEl.innerHTML;
+
+wallPenetrationToggle.onclick = () => {
+	wallPenetration = wallPenetration === Penetration.On ? Penetration.Off : Penetration.On;
+	wallPenetrationEl.innerHTML = wallPenetration;
+};
 
 ball.ondragstart = (): boolean => {
-    return false;
+	return false;
 };
 
 ball.onmousedown = (e: MouseEvent): void => {
-    startDrag(ball, e);
+	startDrag(ball, e);
 };
 
 document.body.onresize = () => {
-    calculatePageBorder();
-    const coords = getCoords(ball);
-    drag(ball, {
-        x: coords.left,
-        y: coords.top,
-    }, {x: 0, y: 0})
+	calculatePageBorder();
+	const coords = getCoords(ball);
+	const validCoords = validateCoordsByPage(ball, coords);
+	if (validCoords.reachedSide || validCoords.reachedUpside) {
+		move(ball, validCoords, {x: 0, y: 0});
+	}
 };
 
 window.onload = (): void => {
-    calculatePageBorder();
-    drag(ball, {
-        x: pageBorder.right / 2 - ball.offsetWidth,
-        y: pageBorder.bottom / 2 - ball.offsetHeight,
-    }, {x: 0, y: 0})
+	calculatePageBorder();
+	move(ball, {
+		x: pageBorder.right / 2 - ball.offsetWidth,
+		y: pageBorder.bottom / 2 - ball.offsetHeight,
+	}, {x: 0, y: 0});
+
+	startFalling(ball);
+	rotate(ball, rotationDeg);
 };
